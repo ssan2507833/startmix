232874a5560d7adafb1caa8bf66b69782495dcc4
ss12932 <ss12932@my.bristol.ac.uk>
2024-11-19 14:54:32 +0000

    feat: implement navbar animation

--- app/routes/_app+/_layout.tsx
+++ app/routes/_app+/_layout.tsx
@@ -1,13 +1,11 @@
 import { Outlet } from "@remix-run/react";
-import Navbar from "#app/components/navbar.tsx";
+import Header from "#app/components/header.tsx";
 
 export default function App() {
   return (
     <>
       <div className="flex min-h-screen flex-col justify-between">
-        <header className="container">
-          <Navbar />
-        </header>
+        <Header />
         <div className="flex-1">
           <Outlet />
         </div>
